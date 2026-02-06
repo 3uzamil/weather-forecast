285c3f6a192788daee23ccc2157ac1637423f4e8
Muzamil Hussain <3uzamilhussain@gmail.com>
2026-02-06 22:09:17 +0500

    fix: bug

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ weatherForm.addEventListener("submit", (e) => {
   }
 });
 
-// weatherForecast("Sukkur");
-
 function weatherForecast(city) {
   let apiUrl = `https://api.weatherapi.com/v1/current.json?key=414bfed570924542aff45116262701&q=${city}`;
   fetch(apiUrl)
